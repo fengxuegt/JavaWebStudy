3a78cb0daff0a78e58295b95ea282a11b3acbeb7
fengxuegt <fengxuegt@163.com>
2023-06-08 00:24:48 +0800

    create Servlet01 module

---
+++ Servlet01/src/main/java/com/lw/MyHttpServlet.java
@@ -0,0 +1,18 @@
+package com.lw;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyHttpServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println("<h1> Welcome liwang </h1>");
+    }
+}
---
+++ Servlet01/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<a href="/Servlet01/welcome"> click me</a>
+</body>
+</html>
